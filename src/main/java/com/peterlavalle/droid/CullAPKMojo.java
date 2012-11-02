cece5d099b47a2d809ccc2d9242ff8384d55c1e2
Peter LaValle <peter.lavalle@gmail.com>
2012-11-02 17:48:05 +0000

    tried to handle compression - doesn't seem to work

--- src/main/java/com/peterlavalle/droid/CullAPKMojo.java
+++ src/main/java/com/peterlavalle/droid/CullAPKMojo.java
@@ -11,8 +11,6 @@ import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 import java.util.zip.ZipEntry;
 import java.util.zip.ZipFile;
 import java.util.zip.ZipOutputStream;
@@ -31,7 +29,7 @@ public class CullAPKMojo extends AbstractDroidMojo {
 	@Override
 	public void execute() throws MojoExecutionException, MojoFailureException {
 
-		final ZipFile apkFile = getApkZipFile();
+		final ZipFile apkZipFile = getApkZipFile();
 
 		// start a new zip output file (on max compression)
 		final File tempFile;
@@ -39,42 +37,51 @@ public class CullAPKMojo extends AbstractDroidMojo {
 		try {
 			tempFile = File.createTempFile("temp-droid-cull", ".zip");
 			zipOutputStream = new ZipOutputStream(new FileOutputStream(tempFile));
-			zipOutputStream.setLevel(9);
 		} catch (IOException ex) {
 			throw new MojoExecutionException("Problem while trying to open temporary file", ex);
 		}
 
-		for (final ZipEntry entry : Util.toIterable(apkFile.entries())) {
-			if (!new File(getAssetsFolder(), entry.getName()).exists() && entry.getName().matches(getAssetsCriteria()) && apkFile.getEntry("assets/" + entry.getName()) != null) {
-				try {
-					// put the entry
-					zipOutputStream.putNextEntry(new ZipEntry(entry.getName()));
-				} catch (IOException ex) {
-					throw new MojoExecutionException("Problem putting entry for `" + entry.getName() + "`", ex);
-				}
+		for (final ZipEntry entry : Util.toIterable(apkZipFile.entries())) {
 
-				// open the input stream
-				final InputStream inputStream;
-				try {
-					inputStream = apkFile.getInputStream(entry);
-				} catch (IOException ex) {
-					throw new MojoExecutionException("Problem opening entry `" + entry.getName() + "`", ex);
-				}
-				try {
-					// copy the streams
-					Util.copyStream(inputStream, zipOutputStream);
-				} catch (IOException ex) {
-					throw new MojoExecutionException("Problem copying entry `" + entry.getName() + "`", ex);
-				}
 
-				// close the entry
-				try {
-					zipOutputStream.closeEntry();
-				} catch (IOException ex) {
-					throw new MojoExecutionException("Problem closing entry `" + entry.getName() + "`", ex);
-				}
+			final String name = entry.getName();
+			final boolean isAsset = name.startsWith("assets/") && name.substring("assets/".length()).matches(getAssetsCriteria());
 
+			// determin if it's a duplicate
+			if (name.matches(getAssetsCriteria()) && apkZipFile.getEntry("assets/" + name) != null) {
 				getLog().info("I removed the duplicate `" + entry.getName() + "`");
+				continue;
+			}
+
+			// set the compression level (assets can't be compressed see - http://ponystyle.com/blog/2010/03/26/dealing-with-asset-compression-in-android-apps/)
+			zipOutputStream.setLevel(isAsset ? 0 : 9);
+
+			// put the entry
+			try {
+				zipOutputStream.putNextEntry(new ZipEntry(entry.getName()));
+			} catch (IOException ex) {
+				throw new MojoExecutionException("Problem putting entry for `" + entry.getName() + "`", ex);
+			}
+
+			// open the input stream
+			final InputStream inputStream;
+			try {
+				inputStream = apkZipFile.getInputStream(entry);
+			} catch (IOException ex) {
+				throw new MojoExecutionException("Problem opening entry `" + entry.getName() + "`", ex);
+			}
+			try {
+				// copy the streams
+				Util.copyStream(inputStream, zipOutputStream);
+			} catch (IOException ex) {
+				throw new MojoExecutionException("Problem copying entry `" + entry.getName() + "`", ex);
+			}
+
+			// close the entry
+			try {
+				zipOutputStream.closeEntry();
+			} catch (IOException ex) {
+				throw new MojoExecutionException("Problem closing entry `" + entry.getName() + "`", ex);
 			}
 		}
 		try {
