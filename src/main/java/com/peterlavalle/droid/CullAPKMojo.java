25f62108aeff3148a3131665e801af8530d996d9
Peter LaValle <peter.lavalle@gmail.com>
2012-11-03 19:18:25 +0000

    found a way

--- src/main/java/com/peterlavalle/droid/CullAPKMojo.java
+++ src/main/java/com/peterlavalle/droid/CullAPKMojo.java
@@ -11,6 +11,7 @@ import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
+import java.util.LinkedList;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import java.util.zip.ZipEntry;
@@ -18,6 +19,9 @@ import java.util.zip.ZipFile;
 import java.util.zip.ZipOutputStream;
 import org.apache.maven.plugin.MojoExecutionException;
 import org.apache.maven.plugin.MojoFailureException;
+import org.codehaus.plexus.util.cli.CommandLineException;
+import org.codehaus.plexus.util.cli.CommandLineUtils;
+import org.codehaus.plexus.util.cli.Commandline;
 
 /**
  *
@@ -28,84 +32,37 @@ import org.apache.maven.plugin.MojoFailureException;
  */
 public class CullAPKMojo extends AbstractDroidMojo {
 
+	/**
+	 * @parameter expression="${env.ANDROID_HOME}"
+	 * @required
+	 */
+	private File androidHome;
+
 	@Override
 	public void execute() throws MojoExecutionException, MojoFailureException {
 
 		final ZipFile apkZipFile = getApkZipFile();
 
-		// start a new zip output file (on max compression)
-		final File tempFile;
-		final ZipOutputStream zipOutputStream;
-		try {
-			tempFile = File.createTempFile("temp-droid-cull", ".zip");
-			zipOutputStream = new ZipOutputStream(new FileOutputStream(tempFile));
-		} catch (IOException ex) {
-			throw new MojoExecutionException("Problem while trying to open temporary file", ex);
-		}
+		final File aapt = new File(androidHome, "platform-tools/aapt.exe");
+
+		final LinkedList<String> removeables = new LinkedList<String>();
 
 		for (final ZipEntry entry : Util.toIterable(apkZipFile.entries())) {
 
 
 			final String name = entry.getName();
-			final boolean isAsset = name.startsWith("assets/") && name.substring("assets/".length()).matches(getAssetsCriteria());
+
 
 			// determin if it's a duplicate
 			if (name.matches(getAssetsCriteria()) && apkZipFile.getEntry("assets/" + name) != null) {
-				getLog().info("I removed the duplicate `" + entry.getName() + "`");
-				continue;
-			}
 
-			// set the compression level (assets can't be compressed see - http://ponystyle.com/blog/2010/03/26/dealing-with-asset-compression-in-android-apps/)
-			zipOutputStream.setLevel(isAsset && entry.getSize() >= (1024 * 1024) ? 0 : 9);
 
-			// put the entry
-			try {
-				zipOutputStream.putNextEntry(new ZipEntry(entry.getName()));
-			} catch (IOException ex) {
-				throw new MojoExecutionException("Problem putting entry for `" + entry.getName() + "`", ex);
-			}
+				getLog().info("I will remove the duplicate `" + entry.getName() + "`");
 
-			// open the input stream
-			final InputStream inputStream;
-			try {
-				inputStream = apkZipFile.getInputStream(entry);
-			} catch (IOException ex) {
-				throw new MojoExecutionException("Problem opening entry `" + entry.getName() + "`", ex);
-			}
-			try {
-				// copy the streams
-				Util.copyStream(inputStream, zipOutputStream);
-			} catch (IOException ex) {
-				throw new MojoExecutionException("Problem copying entry `" + entry.getName() + "`", ex);
-			}
-
-			// close the entry
-			try {
-				zipOutputStream.closeEntry();
-			} catch (IOException ex) {
-				throw new MojoExecutionException("Problem closing entry `" + entry.getName() + "`", ex);
+				removeables.add(name);
 			}
 		}
-		try {
-			zipOutputStream.close();
-		} catch (IOException ex) {
-			throw new MojoExecutionException("IOException while trying to close the zipfile", ex);
-		}
-
-		// copy the old apk file to ".original"
-	//	try {
-	//		Util.copyStream(new FileInputStream(getBuiltFile(".original")), new FileOutputStream(tempFile)).close();
-	//	} catch (IOException ex) {
-	//		throw new MojoExecutionException("IOException while trying to copy original", ex);
-	//	}
-
-		// copy the new zip file as the apk file
-		try {
-			Util.copyStream(new FileInputStream(tempFile), new FileOutputStream(getBuiltFile())).close();
-		} catch (FileNotFoundException ex) {
-			throw new MojoExecutionException("Inexplicable FNF exception", ex);
-		} catch (IOException ex) {
-			throw new MojoExecutionException("IOException while trying to finish the droid:cull", ex);
-		}
+		
+		throw new MojoExecutionException("You must run the command: \n\n\taapt r \"" + getBuiltFile().getAbsolutePath() + " " + removeables.toString().replaceAll("[\\[\\],]", "")+"\n");
 	}
 }
