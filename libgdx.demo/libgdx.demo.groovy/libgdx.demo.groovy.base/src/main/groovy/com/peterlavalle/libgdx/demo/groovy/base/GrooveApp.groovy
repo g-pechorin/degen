155e43852d18d559982011252e6de0e208e6dc97
Peter LaValle <peter dot lavalle at gmail dot com>
2013-04-24 02:01:43 +0100

    start work on Groovy project and GDXDroidMojo

---
+++ libgdx.demo/libgdx.demo.groovy/libgdx.demo.groovy.base/src/main/groovy/com/peterlavalle/libgdx/demo/groovy/base/GrooveApp.groovy
@@ -0,0 +1,13 @@
+package com.peterlavalle.libgdx.demo.groovy.base
+
+import sun.net.www.ApplicationLaunchException
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: peter
+ * Date: 24/04/13
+ * Time: 01:46
+ * To change this template use File | Settings | File Templates.
+ */
+class GrooveApp implements ApplicationListener {
+}
